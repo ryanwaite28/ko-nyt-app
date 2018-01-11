53ef057240c5b76389810048f5ac68db27d96234
Waite <Waite.Ryan.M@dol.gov>
2018-01-11 12:37:00 -0500

    initial

---
+++ README.md
@@ -0,0 +1,32 @@
+# Knockout JS
+![](img/knockout.png)
+![](img/nytimes.jpg)
+
+Knockout Website: http://knockoutjs.com/
+<br/>
+New York Times API Website: https://developer.nytimes.com/
+
+<blockquote>
+Simplify dynamic JavaScript UIs with the Model-View-View Model (MVVM) pattern.
+<br/><br/>
+By encapsulating data and behavior into a view model, you get a clean, extensible foundation on which to build sophisticated UIs without getting lost in a tangle of event handlers and manual DOM updates.
+<br/><br/><br/>
+-- Knockout
+</blockquote>
+
+<blockquote>
+The New York Times Developer Network<br/>
+All the APIs Fit to POST
+<br/><br/>
+You already know that NYTimes.com is an unparalleled source of news and information. But now it's a premier source of data, too — why just read the news when you can hack it?
+<br/><br/><br/>
+-- New York Times
+</blockquote>
+
+A web app written with Knockout JS and uses the NY Times API. <br/>
+Demonstrates the understanding of the MVC (Model | View | Controller) design pattern.
+
+<br/>
+
+### Ryan M. Waite
+Developer/Programmer
---
+++ script.js
@@ -0,0 +1,70 @@
+// view model
+
+function MVC() {
+  var self = this;
+  window.logMVC = function() { console.log('mvc', self); }
+
+  var api_key = '52ed7b1a514043208bd970208177f48c';
+  self.nytimes_domain = 'https://www.nytimes.com/';
+
+  self.name = ko.observable('');
+  self.query = ko.observable('');
+  self.requestMSG = ko.observable('');
+  self.list = ko.observableArray([]);
+
+  self.uniqueValue = function() {
+    return String(Date.now()) + Math.random().toString(36).substr(2, 34);
+  }
+
+  self.clearList = function() {
+    self.list( [] );
+  }
+
+  self.getArticles = function() {
+    let query = self.query();
+
+    if(query == "") {
+      alert('Query cannot be empty');
+      return;
+    }
+    if(query.length < 3) {
+      alert('Query must be a minimum of 3 characters');
+      return;
+    }
+
+    var nytURL = 'https://api.nytimes.com/svc/search/v2/articlesearch.json?q=' + query + '&sort=newest&api-key=' + api_key;
+    console.log('request: ', nytURL);
+    self.requestMSG('Loading...');
+
+    fetch(nytURL)
+    .then(function(resp) { return resp.json(); })
+    .then(function(json) {
+      console.log('resp', json);
+      if(json.response.docs && json.response.docs.length > 0){
+        self.list( json.response.docs );
+        self.requestMSG('Successfully loaded articles about ' + query + '!');
+      }
+      else {
+        self.requestMSG('No Results...');
+      }
+    })
+    .catch(function() {
+      self.requestMSG('Load was unsuccessful...');
+    });
+  }
+
+  //
+
+  $(document).ready(function() {
+    $('#query-input').keyup(function(e) {
+      if(e.keyCode === 13) {
+        self.getArticles();
+      }
+    });
+  });
+}
+
+// init
+const mvc = new MVC();
+ko.applyBindings(mvc);
+Object.freeze(mvc);
